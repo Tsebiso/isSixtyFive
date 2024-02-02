4c6ddfee6ecfc085a5f967d963db35831deead38
Tsebiso <tsebisonzinzama@gmail.com>
2024-02-02 15:52:48 +0200

    isSixtyFive"

---
+++ src/isSixtyFive.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+   public class isSixtyFive {
+    public static boolean isSixtyFive(int num1, int num2) {
+        return num1 == 65 || num2 == 65 || (num1 + num2) == 65;
+    }
+
+    public static void main(String[] args) {
+        
+        boolean result1 = isSixtyFive(20, 25);
+        System.out.println(result1);  
+
+        boolean result2 = isSixtyFive(15, 30);
+        System.out.println(result2);  
+    }
+}
+
+    
+
